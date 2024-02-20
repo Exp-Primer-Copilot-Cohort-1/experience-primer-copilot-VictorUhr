7d23f1a1e752986fbf0166e40a2c491db224c4d1
VictorUhr <106089762+VictorUhr@users.noreply.github.com>
2024-02-20 01:54:33 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,105 @@
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+```
+
+```javascript
+// Path: comments.js
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+```
+
+```javascript
+// Path: comments.js
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+```
+
+```javascript
+// Path: comments.js
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+```
+
+```javascript
+// Path: comments.js
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+```
+
+```javascript
+// Path: comments.js
+// Create web server
+// Run server
+// Open browser and navigate to http://localhost:3000
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+
+
